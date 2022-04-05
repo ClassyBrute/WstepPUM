1ef63bc2f8745ef52728d50f98d8c8623c4360bf
unknown <315696@uwr.edu.pl>
2022-04-05 17:17:58 +0200

    zadanie2

--- lista3/zad2.kt
+++ lista3/zad2.kt
@@ -1,5 +1,8 @@
 package lista3
 
+import kotlin.math.abs
+import kotlin.math.max
+
 fun main() {
     tally()
 }
@@ -8,36 +11,60 @@ fun tally() {
     val candidates = mutableMapOf("Rafal" to 0, "Robert" to 0, "Ania" to 0)
 
     // which one is the preferred candidate
-    val rafa_ania = mutableMapOf("Rafal" to 0, "Ania" to 0)
-    val rafa_robe = mutableMapOf("Rafal" to 0, "Robert" to 0)
-    val robe_ania = mutableMapOf("Robert" to 0, "Ania" to 0)
+    val rafa_ania = mutableMapOf("Rafal" to 0, "Ania" to 0, "str" to 0)
+    val rafa_robe = mutableMapOf("Rafal" to 0, "Robert" to 0, "str" to 0)
+    val robe_ania = mutableMapOf("Robert" to 0, "Ania" to 0, "str" to 0)
+
+    val pairs = mutableListOf(rafa_ania, rafa_robe, robe_ania)
 
     for (i in 0..8) {                              // 9 voters
         val shuffled = candidates.keys.shuffled()  // simulating voter choices
 
-//        println(shuffled)
+        println(shuffled)
 
         for (name in shuffled) {
-            val rank = shuffled.size - shuffled.indexOf(name)   // how many points based on position
-            candidates[name] = rank
-
-//            println(candidates)
+            candidates[name] = shuffled.size - shuffled.indexOf(name)   // how many points based on position
         }
 
-        if (candidates["Rafal"]!! > candidates["Ania"]!!) {
-            rafa_ania["Rafal"] = rafa_ania["Rafal"]!!.plus(1)
-        } else rafa_ania["Ania"] = rafa_ania["Ania"]!!.plus(1)
+        win(candidates, rafa_ania, "Rafal", "Ania")
+        win(candidates, rafa_robe, "Rafal", "Robert")
+        win(candidates, robe_ania, "Robert", "Ania")
 
-        if (candidates["Rafal"]!! > candidates["Robert"]!!) {
-            rafa_robe["Rafal"] = rafa_robe["Rafal"]!!.plus(1)
-        } else rafa_robe["Robert"] = rafa_robe["Robert"]!!.plus(1)
-
-        if (candidates["Robert"]!! > candidates["Ania"]!!) {
-            robe_ania["Robert"] = robe_ania["Robert"]!!.plus(1)
-        } else robe_ania["Ania"] = robe_ania["Ania"]!!.plus(1)
 
         println(rafa_ania)
         println(rafa_robe)
         println(robe_ania)
     }
+
+    rafa_ania["str"] = abs(rafa_ania["Rafal"]!! - rafa_ania["Ania"]!!)
+    rafa_robe["str"] = abs(rafa_robe["Rafal"]!! - rafa_robe["Robert"]!!)
+    robe_ania["str"] = abs(robe_ania["Robert"]!! - robe_ania["Ania"]!!)
+
+    println(rafa_ania)
+    println(rafa_robe)
+    println(robe_ania)
+
+    val strongestDiff = maxOf(rafa_ania["str"]!!, rafa_robe["str"]!!, robe_ania["str"]!!)
+
+    pairs.find {  }
+
+    println(strongestDiff)
+
+
+
+
+    val graph = mutableListOf<String>()
+
+
+
+}
+
+fun win(candidates: MutableMap<String, Int>,
+        pair: MutableMap<String, Int>,
+        first: String,
+        second: String) {
+    if (candidates[first]!! > candidates[second]!!) {
+        pair[first] = pair[first]!!.plus(1)
+    } else pair[second] = pair[second]!!.plus(1)
+
 }
