b385fa2260d39f01adc8da8a80ce85e89819f777
unknown <315696@uwr.edu.pl>
2022-04-12 17:11:36 +0200

    todo zad5

--- lista4/zad6.kt
+++ lista4/zad6.kt
@@ -1,15 +1,13 @@
 package lista4
 
+val <T> List<T>.head
+    get() = first()
+
+val <T> List<T>.tail
+    get() = last()
+
 fun main() {
-    fun List<Int>.tail(): List<Int> {
-        return this.drop(1)
-    }
-
-    fun List<Int>.head(): Int {
-        return this[0]
-    }
-
     val numbers = (1..5).toList()
-    println(numbers.tail())
-    println(numbers.head())
+    println(numbers.tail)
+    println(numbers.head)
 }
--- lista4/zad8.kt
+++ lista4/zad8.kt
@@ -1,17 +1,12 @@
 package lista4
 
 fun main() {
-//    fun <A> suma(array: Array<A>): Double {
-//        var sum = 0
-//        for (i in array) {
-//            if (i > 0) {
-//               sum += i
-//            }
-//        }
-//    // TODO
-//        return array
-//    }
-//
-//    val array = arrayOf(1, -4.0, 12, 0, -3, 29, -150)
-//    suma(array)
+    fun suma(array: Array<Int>): Int {
+        return array.map { it }
+            .filter { it > 0 }
+            .reduce { sum, element -> sum + element }
+    }
+
+    val array = arrayOf(1, -4, 12, 0, -3, 29, -150)
+    println(suma(array))
 }
